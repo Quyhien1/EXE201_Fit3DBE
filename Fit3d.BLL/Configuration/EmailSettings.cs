9d0c32b10234786f7571d4e33487905bc88be504
Hien <hiennpqse180072@fpt.edu.vn>
2026-03-14 22:57:03 +0700

    change to other email sending service

--- Fit3d.BLL/Configuration/EmailSettings.cs
+++ Fit3d.BLL/Configuration/EmailSettings.cs
@@ -2,12 +2,11 @@ namespace Fit3d.BLL.Configuration
 {
     public class EmailSettings
     {
-        public string SmtpHost { get; set; } = string.Empty;
-        public int SmtpPort { get; set; } = 587;
+        public bool UseHttpApi { get; set; } = true;
+        public string ApiUrl { get; set; } = "https://api.resend.com/emails";
+        public string ApiKey { get; set; } = string.Empty;
         public string SenderName { get; set; } = string.Empty;
         public string SenderEmail { get; set; } = string.Empty;
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-        public bool UseSsl { get; set; } = true;
+        public int TimeoutSeconds { get; set; } = 20;
     }
 }
